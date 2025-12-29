e3f19e26070d04bd877a92f7891cc33c32be1dd3
weeger <romain.weeger@gmail.com>
2025-12-29 15:18:30 +0100

    Move demo and test

--- assets/js/Class/TestApp.ts
+++
@@ -1,9 +0,0 @@
-import App from './App';
-import AppService from './AppService';
-import VueService from '../Services/VueService';
-
-export default class extends App {
-  getServices(): (typeof AppService | [typeof AppService, any[]])[] {
-    return [...super.getServices(), ...[VueService]];
-  }
-}
