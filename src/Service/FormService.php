3874a68509e074f740b8f45a05b3e759770a97eb
weeger <romain.weeger@gmail.com>
2026-02-05 17:53:20 +0100

    Form as component

---
+++ src/Service/FormService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Wexample\SymfonyLoader\Service;
+
+use Exception;
+use Twig\Environment;
+use Symfony\Component\Form\FormView;
+use Wexample\SymfonyLoader\Rendering\RenderNode\ComponentRenderNode;
+use Wexample\SymfonyLoader\Rendering\RenderPass;
+
+class FormService extends ComponentService
+{
+    /**
+     * Render a form template with the component pipeline so assets/domain handling
+     * can be unified with components.
+     *
+     * @throws Exception
+     */
+    public function formLoad(
+        Environment $twig,
+        RenderPass $renderPass,
+        FormView $formView,
+        string $path,
+        array $options = []
+    ): ComponentRenderNode {
+        $options['form'] = $formView;
+
+        return $this->componentInitPrevious(
+            $twig,
+            $renderPass,
+            $path,
+            $options
+        );
+    }
+}
