688aa4bd7a89e0e5096a7b9b2add6beae6d208b3
weeger <romain.weeger@gmail.com>
2026-02-05 17:51:38 +0100

    Form as component

--- src/Twig/FormExtension.php
+++ src/Twig/FormExtension.php
@@ -2,11 +2,22 @@
 
 namespace Wexample\SymfonyLoader\Twig;
 
+use Exception;
+use Symfony\Component\Form\FormView;
+use Twig\Environment;
 use Twig\TwigFunction;
 use Wexample\SymfonyHelpers\Twig\AbstractExtension;
+use Wexample\SymfonyLoader\Rendering\RenderPass;
+use Wexample\SymfonyLoader\Service\FormService;
 
 class FormExtension extends AbstractExtension
 {
+    public function __construct(
+        private readonly FormService $formService
+    )
+    {
+    }
+
     public function getFunctions(): array
     {
         return [
@@ -15,13 +26,34 @@ class FormExtension extends AbstractExtension
                 [
                     $this,
                     'formLoad',
+                ],
+                [
+                    self::FUNCTION_OPTION_IS_SAFE => self::FUNCTION_OPTION_IS_SAFE_VALUE_HTML,
+                    self::FUNCTION_OPTION_NEEDS_ENVIRONMENT => true,
                 ]
             ),
         ];
     }
 
-    public function formLoad(): string
+    /**
+     * @throws Exception
+     */
+    public function formLoad(
+        Environment $twig,
+        RenderPass $renderPass,
+        FormView $formView,
+        string $path,
+        array $options = []
+    ): string
     {
-        return '';
+        $component = $this->formService->formLoad(
+            $twig,
+            $renderPass,
+            $formView,
+            $path,
+            $options
+        );
+
+        return $component->getBody() . $component->renderTag();
     }
 }
