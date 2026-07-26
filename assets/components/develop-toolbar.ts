50f1770c1f08fe34e002386fc315d19ea192b0ba
weeger <romain.weeger@gmail.com>
2026-07-26 15:12:53 +0200

    Cleanup

--- assets/components/develop-toolbar.ts
+++ assets/components/develop-toolbar.ts
@@ -1,15 +1,3 @@
 import Component from '../js/Class/Component';
-import VueService from '../js/Services/VueService';
 
-export default class extends Component {
-  protected async activateListeners(): Promise<void> {
-    const mountEl = this.el.querySelector<HTMLElement>('.develop-toolbar__mount');
-    if (mountEl && this.app.services.vue) {
-      const vueService = this.app.services.vue as VueService;
-      const component = vueService.initComponent('@WexampleSymfonyLoaderBundle/vue/debug-assets', this);
-      vueService.createApp(component as Record<string, any>).mount(mountEl);
-    }
-  }
-
-  protected async deactivateListeners(): Promise<void> {}
-}
+export default class extends Component {}
