4aad3769972817d43e2bf1294ef0ad2f2d852175
weeger <romain.weeger@gmail.com>
2026-07-27 15:00:23 +0200

    Cleanup

--- assets/components/develop-toolbar.ts
+++ assets/components/develop-toolbar.ts
@@ -1,6 +1,9 @@
 import Component from '../js/Class/Component';
 import DebugService from '../js/Services/DebugService';
 
+const HEIGHT_VAR = '--develop-toolbar-height';
+const SF_TOOLBAR_SELECTOR = '.sf-toolbar';
+
 export default class DevelopToolbar extends Component {
   private activeTab: string = 'assets';
   private debugInitialized: boolean = false;
@@ -14,6 +17,57 @@ export default class DevelopToolbar extends Component {
         this.switchTab(el.dataset.tab);
       });
     });
+
+    this.el.querySelector('[data-symfony-toolbar-toggle]')?.addEventListener('click', (e) => {
+      e.preventDefault();
+      this.toggleSymfonyToolbar();
+    });
+
+    this.initResize();
+    this.initHeightObserver();
+  }
+
+  private toggleSymfonyToolbar() {
+    const sf = document.querySelector<HTMLElement>(SF_TOOLBAR_SELECTOR);
+    if (sf) {
+      sf.style.display = sf.style.display === 'none' ? '' : 'none';
+    }
+  }
+
+  private initResize() {
+    const handle = this.el.querySelector<HTMLElement>('.develop-toolbar__resize-handle');
+    if (!handle) return;
+
+    handle.addEventListener('mousedown', (e) => {
+      e.preventDefault();
+      const startY = e.clientY;
+      const startHeight = this.el.offsetHeight;
+
+      const onMove = (e: MouseEvent) => {
+        const newHeight = Math.max(80, startHeight - (e.clientY - startY));
+        this.el.style.height = `${newHeight}px`;
+        this.updateHeightVar(newHeight);
+      };
+
+      const onUp = () => {
+        document.removeEventListener('mousemove', onMove);
+        document.removeEventListener('mouseup', onUp);
+      };
+
+      document.addEventListener('mousemove', onMove);
+      document.addEventListener('mouseup', onUp);
+    });
+  }
+
+  private initHeightObserver() {
+    new ResizeObserver((entries) => {
+      const height = entries[0]?.borderBoxSize[0]?.blockSize ?? this.el.offsetHeight;
+      this.updateHeightVar(height);
+    }).observe(this.el);
+  }
+
+  private updateHeightVar(height: number) {
+    document.documentElement.style.setProperty(HEIGHT_VAR, `${height}px`);
   }
 
   private async switchTab(tab: string) {
